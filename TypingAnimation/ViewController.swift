d72381981c4b58bbb9b9f5b3542cd9759e8c8ba7
Damodar Shenoy <itsdamslife@gmail.com>
2019-01-16 23:27:11 +0530

    Used Timer as requested by youtube commenter

--- TypingAnimation/ViewController.swift
+++ TypingAnimation/ViewController.swift
@@ -30,10 +30,32 @@ class ViewController: UIViewController {
         animatingLabel.text = ""
         
         let appName = (Bundle.main.infoDictionary?["CFBundleName"] as? String) ??  "itscoderslife();"
+
+        // New code using Timer class
+        
+        let characters = appName.map { $0 }
+        var index = 0
+        Timer.scheduledTimer(withTimeInterval: 0.12, repeats: true, block: { [weak self] timer in
+            if index < appName.count {
+                let char = characters[index]
+                self?.animatingLabel.text! += "\(char)"
+                index += 1
+            } else {
+                timer.invalidate()
+            }
+        })
+
+        /**
+
+        // Older Code using Run loop to block the UI
+
         for char in appName {
             animatingLabel.text! += "\(char)"
             RunLoop.current.run(until: Date()+0.12)
         }
+
+        */
+
     }
 }
 
